39584b1ba1163bad9795ef9a852a4df8d6548834
elezboris99 <elez.boris@gmail.com>
2023-11-04 15:03:11 +0100

    promjene za logovanje

--- store/app/Http/Controllers/API/AuthController.php
+++ store/app/Http/Controllers/API/AuthController.php
@@ -37,11 +37,11 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            return response()->json(['success'=>false]);
         }
         $user = User::where('email', $request['email'])->firstOrFail();
         $token = $user->createToken('auth_token')->plainTextToken;
-        return response()->json(['message' => 'Pozdrav ' . $user->first_name . ', dobrodosli ', 'access_token' => $token, 'token_type' => 'Bearer',]);
+        return response()->json(['success'=>true, 'access_token' => $token, 'token_type' => 'Bearer',]);
     }
 
     public function logout()
